ddfc5c1b0acddd00b3ee880fe4e72e1eacb451b1
puyantao <puyantao123759>
2019-07-09 15:45:39 +0800

    first commit

---
+++ z_lib_persion/src/main/java/com/example/z_lib_persion/ShareActivity.java
@@ -0,0 +1,19 @@
+package com.example.z_lib_persion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ *
+ * Description
+ * Author puyantao
+ * Email 1067899750@qq.com
+ * Date 2019/7/9 14:37
+ */
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_activity_share);
+    }
+}
---
+++ z_lib_video/src/main/java/com/example/z_lib_video/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.z_lib_video;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ *
+ * Description
+ * Author puyantao
+ * Email 1067899750@qq.com
+ * Date 2019/7/9 14:25
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_activity_main);
+    }
+}
