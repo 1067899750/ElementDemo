5c94529e4db403bfacfd0192cc0d69a59665a570
puyantao <puyantao123759>
2019-11-06 16:51:27 +0800

    # design

--- moduleCore/z_lib_news/src/main/java/com/example/z_lib_news/fragment/attention/AttentionFragment.java
+++ moduleCore/z_lib_news/src/main/java/com/example/z_lib_news/fragment/attention/AttentionFragment.java
@@ -1,17 +1,12 @@
 package com.example.z_lib_news.fragment.attention;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.z_lib_common.base.mvc.fragment.BaseMvcActionBarFragment;
 import com.example.z_lib_common.base.mvc.fragment.BaseMvcFragment;
 import com.example.z_lib_news.R;
 import com.google.gson.JsonObject;
--- moduleCore/z_lib_news/src/main/java/com/example/z_lib_news/fragment/me/MeFragment.java
+++ moduleCore/z_lib_news/src/main/java/com/example/z_lib_news/fragment/me/MeFragment.java
@@ -1,12 +1,8 @@
 package com.example.z_lib_news.fragment.me;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.z_lib_common.base.mvc.fragment.BaseMvcActionBarFragment;
 import com.example.z_lib_news.R;
