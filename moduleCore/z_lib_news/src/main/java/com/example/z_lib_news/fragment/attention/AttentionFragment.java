8470f7fbb0e6f592d9b63bb247aa4753e00d6e17
puyantao <puyantao123759>
2019-11-06 16:50:43 +0800

    # design

--- moduleCore/z_lib_news/src/main/java/com/example/z_lib_news/fragment/attention/AttentionFragment.java
+++ moduleCore/z_lib_news/src/main/java/com/example/z_lib_news/fragment/attention/AttentionFragment.java
@@ -3,12 +3,16 @@ package com.example.z_lib_news.fragment.attention;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.z_lib_common.base.mvc.fragment.BaseMvcActionBarFragment;
+import com.example.z_lib_common.base.mvc.fragment.BaseMvcFragment;
 import com.example.z_lib_news.R;
 import com.google.gson.JsonObject;
 
@@ -18,7 +22,10 @@ import com.google.gson.JsonObject;
  * @author puyantao
  * @date 2019/11/6 16:16
  */
-public class AttentionFragment extends BaseMvcActionBarFragment {
+public class AttentionFragment extends BaseMvcFragment {
+    private FloatingActionButton mFloatingActionButton;
+    private TextView mTextView;
+    private ImageView mImageView;
 
     public AttentionFragment() {
         // Required empty public constructor
@@ -46,7 +53,9 @@ public class AttentionFragment extends BaseMvcActionBarFragment {
 
     @Override
     protected void initViews(View root) {
-
+        mFloatingActionButton = root.findViewById(R.id.fab_icon);
+        mTextView = root.findViewById(R.id.tv_content);
+        mImageView = root.findViewById(R.id.iv_main_backdrop);
     }
 
     @Override
@@ -54,10 +63,6 @@ public class AttentionFragment extends BaseMvcActionBarFragment {
 
     }
 
-    @Override
-    protected String setTitle() {
-        return "关注";
-    }
 
     @Override
     protected void onSuccess(String tag, JsonObject jsonObject) {
